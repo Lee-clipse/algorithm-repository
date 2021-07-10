779be86ee070f327af0a155f43e6f0ef61283855
edac99 <79911816+edac99@users.noreply.github.com>
2021-07-10 09:56:47 +0900

    Add files via upload

--- DP_11052.cpp
+++ DP_11052.cpp
@@ -4,52 +4,34 @@
 using namespace std;
 
 const int maxN = 1000;
+const int MOD  = 10007;
 
 int N, K;
-int P[maxN+1];
-int dp[maxN+1];
+int dp[maxN+1][maxN+1];
 
 void Input ()
 {
-    memset(dp, -1, sizeof(dp));
+    memset(dp, 0, sizeof(dp));
 
-    cin >> N;
+    cin >> N >> K;
     for (int i = 1; i <= N; i++)
     {
-        cin >> P[i];
+        dp[i][0] = 1;
+        dp[i][i] = 1;
     }
-}
-
-int func (int K)
-{
-    int& ret = dp[K];
-
-    if (K == N)
-    {
-        return 0;
-    }
-
-    if (ret != -1)
-    {
-        return ret;
-    }
-
-    for (int i = 1; i <= N; i++)
-    {
-        if (i > N-K)
-        {
-            break;
-        }
-
-        ret = max(ret, func(K+i) + P[i]);
-    }
-
-    return ret;
 }
 
 void Solution ()
 {
-    cout << func(0);
+    for (int i = 2; i <= N; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            dp[i][j] = (dp[i-1][j] + dp[i-1][j-1]) % MOD;
+        }
+    }
+
+    cout << dp[N][K] << endl;
 }
 
 void Solve ()
