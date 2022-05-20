8acc5af168e93edb95384e9e222f97aaba8c0e60
edac99 <79911816+edac99@users.noreply.github.com>
2022-05-20 21:25:00 +0900

    Add files via upload

--- DP_2579.cpp
+++ DP_2579.cpp
@@ -1,48 +1,27 @@
-#include <iostream>
-#include <cstring>
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
 
 using namespace std;
 
-const int maxN = 300;
+int dp[3][303];
+int arr[303];
 
-int N;
-int arr[maxN+1];
-int dp[maxN+1];
-
-void Input ()
-{
-    memset(dp, 0, sizeof(dp));
-
-    cin >> N;
-    for (int i = 1; i <= N; i++)
-    {
+int main() {
+	fastio;
+    int N; cin >> N;
+    for (int i = 0; i < N; i++) {
         cin >> arr[i];
-    }
-}
-
-void Solution ()
-{
-    dp[1] = arr[1];
-    dp[2] = dp[1] + arr[2];
-
-    for (int i = 3; i <= N; i++)
-    {
-        dp[i] = max(dp[i-3] + arr[i-1] + arr[i],
-                    dp[i-2] + arr[i]);
+        dp[1][i] = dp[2][i] = arr[i];
     }
 
-    cout << dp[N] << endl;
-}
+    dp[2][0] = 0;
+    for (int i = 0; i < N; i++) {
+        dp[1][i+2] = max(dp[1][i+2], dp[1][i] + arr[i+2]);
+        dp[2][i+1] = max(dp[2][i+1], dp[1][i] + arr[i+1]);
+        dp[1][i+2] = max(dp[1][i+2], dp[2][i] + arr[i+2]);
+    }
 
-void Solve ()
-{
-    Input();
-    Solution();
-}
 
-int main ()
-{
-    Solve();
 
-    return 0;
+    cout << max(dp[1][N-1], dp[2][N-1]);
 }
