32ac8fa1d75f47a2bb61a8da2a8843b6a33a60d3
edac99 <79911816+edac99@users.noreply.github.com>
2022-06-10 07:53:47 +0900

    Solved S2_2805
    Used Binary Search algorithm.
    And used long long type beacuse of large input size.

--- Binary Search/S2_2805.cpp
+++ Binary Search/S2_2805.cpp
@@ -3,7 +3,44 @@
 
 using namespace std;
 
+typedef unsigned long long ull;
+
+const int maxN = 1000001;
+
+int N, M;
+int arr[maxN];
+
+ull cutTree(int pos) {
+    ull sum = 0;
+    for (int i = N-1; i >= 0; i--) {
+        if (pos < arr[i]) sum += (arr[i] - pos);
+    }
+    return sum;
+}
+
 int main() {
 	fastio;
+    int lo = 0, hi = 0;
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+        hi = max(hi, arr[i]);
+    }
     
+    int ans;
+    while (lo <= hi) {
+        int mid = (lo + hi) / 2;
+
+        ull tree = cutTree(mid);
+
+        if (M > tree) {
+            hi = mid-1;
+        }
+        else {
+            ans = mid;
+            lo = mid+1;
+        }
+    }
+
+    cout << ans << "\n";
 }
