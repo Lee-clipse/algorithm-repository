6894f4cfab28805b333da46ca543d0ece90cf415
edac99 <79911816+edac99@users.noreply.github.com>
2022-06-11 08:02:29 +0900

    Solved S2_1541.cpp
    Use Greedy algorithm.
    I have to make better structure for clean code.
    Maybe have a better logic.

--- Greedy/S2_1541.cpp
+++ Greedy/S2_1541.cpp
@@ -5,5 +5,44 @@ using namespace std;
 
 int main() {
 	fastio;
-    
+    string str; cin >> str;
+    int str_size = str.size();
+    int sum = 0, num = 0;
+    int idx = 0;
+    for (int i = 0; i < str_size; i++) {
+        if ('0' <= str[i] && str[i] <= '9') {
+            num *= 10; 
+            num += (str[i] - '0');
+        } 
+        else {
+            sum += num;
+            num = 0;
+            if (str[i] == '-') {
+                idx = i+1;
+                break;
+            } 
+        }
+    }
+    if (num != 0) {
+        sum += num;
+        num = 0;
+    }
+    if (idx == 0) {
+        cout << sum << "\n";
+        return 0;
+    }
+    for (int i = idx; i < str_size; i++) {
+        if ('0' <= str[i] && str[i] <= '9') {
+            num *= 10; num += (str[i] - '0');
+        }
+        else {
+            sum -= num;
+            num = 0;
+        }
+    }
+    if (num != 0) {
+        sum -= num;
+    }
+ 
+    cout << sum << "\n";
 }
