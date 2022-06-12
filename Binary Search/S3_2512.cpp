61cd39a2c94339067f385aecea3b24655d6bf222
edac99 <79911816+edac99@users.noreply.github.com>
2022-06-13 07:47:18 +0900

    Solved S3_2512
    Used Binary search algorithm.
    It's similar to S2_2850.cpp.

--- Binary Search/S3_2512.cpp
+++ Binary Search/S3_2512.cpp
@@ -3,7 +3,51 @@
 
 using namespace std;
 
+const int maxN = 10010;
+
+int N, M;
+int arr[maxN];
+
+int getBudget(int pos) {
+    int ans = 0;
+    for (int i = 0; i < N; i++) {
+        if (arr[i] <= pos) {
+            ans += arr[i];
+        }
+        else {
+            ans += pos;
+        }
+    }
+    return ans;
+} 
+
 int main() {
 	fastio;
+    cin >> N;
+    int max_val = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+        max_val = max(max_val, arr[i]);
+    }
+    cin >> M;
     
+    int ans, diff = 100000001;
+    int lo = 0, hi = max_val, mid;
+    while (lo <= hi) {
+        mid = (lo + hi) / 2;
+
+        int budget = getBudget(mid);
+        if (budget > M) {
+            hi = mid-1;
+        } 
+        else {
+            lo = mid+1;
+            if (diff > M - budget) {
+                diff = M - budget;
+                ans = mid;
+            }
+        }
+    }
+
+    cout << ans << "\n";
 }
