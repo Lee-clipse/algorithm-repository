20d0de12449f81511e9af62f0bf4b9486802b4cc
edac99 <79911816+edac99@users.noreply.github.com>
2022-06-16 07:45:09 +0900

    Solved G5.9084
    DP problem.
    Make ignition type and do simulataion.

--- DP/G5_9084.cpp
+++ DP/G5_9084.cpp
@@ -3,7 +3,35 @@
 
 using namespace std;
 
+const int maxN = 10010;
+
+int coin[maxN];
+int dp[maxN];
+
 int main() {
 	fastio;
-    
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            cin >> coin[i];
+        }
+
+        int M;
+        cin >> M;
+
+        memset(dp, 0, sizeof(dp));
+        dp[0] = 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (j >= coin[i]) {
+                    dp[j] += dp[j - coin[i]];
+                }
+            }
+        }
+
+        cout << dp[M] << "\n";
+    }
 }
