ef12aa1b110af030598c5489bdd7397b86f63259
edac99 <79911816+edac99@users.noreply.github.com>
2022-06-17 07:58:37 +0900

    Solved S2_1654.cpp
    Used Binary Search algorithm.
    Because the data size is large, choose the data type carefully.

--- Binary Search/S2_1654.cpp
+++ Binary Search/S2_1654.cpp
@@ -3,7 +3,41 @@
 
 using namespace std;
 
+typedef unsigned long long ull;
+
+const int maxK = 10010;
+
+int arr[maxK];
+
 int main() {
 	fastio;
+    int K, N;
+    cin >> K >> N;
+
+    int max_len = 0;
+    for (int i = 0; i < K; i++) {
+        cin >> arr[i];
+        max_len = max(max_len, arr[i]);
+    }
+
+    ull answer = 0;
+    ull lo = 1, hi = max_len, mid;
+    while (lo <= hi) {
+        mid = (lo + hi) / 2;
+
+        ull cut = 0;
+        for (int i = 0; i < K; i++) {
+            cut += (arr[i] / mid);
+        }
+
+        if (cut < N) {
+            hi = mid - 1;
+        } 
+        else {
+            lo = mid + 1;
+            answer = max(answer, mid);
+        }
+    }
     
+    cout << answer << "\n";
 }
