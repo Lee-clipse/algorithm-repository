d6220dbcb6fcbbdd5cc8257a26e2655cd02b51b1
JinJae Lee <jamjam1208@naver.com>
2022-06-26 16:17:31 +0900

    LV1 3진법 뒤집기 - 큐

--- Implementation/LV1_68935.cpp
+++ Implementation/LV1_68935.cpp
@@ -1,25 +1,23 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <cmath>
+#include <queue>
 
 using namespace std;
-
-// 3:50 ~ 4:00 (10+)
 
 int solution(int n) {
     int answer = 0;
     
-    // k는 n보다 작은 수 중 가장 큰 3의 승수
-    int k = 0;
-    while (pow(3, k++) <= n);
-    k -= 2;
+    queue <int> Q;
     
-    for (int i = 0; i <= k; i++) {
-        
-        // n을 3진법으로 한 번에 만들고 뒤집지 않고 10진법으로 바로 변환
-        answer += ((n / (int)pow(3, k-i)) * (int)pow(3, i));
-        n = n % (int)pow(3, k-i);
+    while (n) {
+        Q.push(n % 3);
+        n /= 3;
+    }
+    
+    while (!Q.empty()) {
+        answer = answer * 3 + Q.front();
+        Q.pop();
     }
     
     return answer;
