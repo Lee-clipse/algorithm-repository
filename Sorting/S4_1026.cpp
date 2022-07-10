88f8ed222e440c5e73194264e571d9f206ae96de
edac99 <79911816+edac99@users.noreply.github.com>
2022-07-10 23:01:23 +0900

    Solved S4_1026

---
+++ Sorting/S4_1026.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+
+bool visited[26] = { 0 };
+
+set<char> arr[51];
+
+int n = 0, k = 0, result = 0;
+
+void dfs(int alpha, int cnt) {
+	if (cnt == (k - 5)) {
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			bool jud = true;
+
+			for (auto it = arr[i].begin(); it != arr[i].end(); it++) {
+				if (visited[*it - 'a'] == 0) {
+					jud = false;
+					break;
+				}
+			}
+			
+			if (jud == true)
+				cnt++;
+		}
+		 result = max(result,cnt);
+
+		return;
+	}
+
+	for (int i = alpha; i < 26; i++) {
+
+		if (visited[i] == 0) {
+			visited[i] = 1;
+			dfs(i,cnt + 1);
+			visited[i] = 0; 
+		}
+	}
+
+}
+
+int main() {
+    fastio;
+
+	cin >> n >> k;
+
+	visited['a' - 'a'] = 1;
+	visited['c' - 'a'] = 1;
+	visited['i' - 'a'] = 1;
+	visited['t' - 'a'] = 1;
+	visited['n' - 'a'] = 1;
+
+	for (int i = 0; i < n; i++) {
+		string v;
+		cin >> v;
+
+		for (int j = 0; j < v.size(); j++) {
+			if (v[j] != 'a' && v[j] != 'c' && v[j] != 'i' && v[j] != 't' && v[j] != 'n') {
+				arr[i].insert(v[j]);
+			}
+		}
+			
+	}
+	
+
+	if (k < 5) {
+		cout << 0 << '\n';
+		return 0;
+	}
+	else if (k == 26) {
+		cout << n << '\n';
+		return 0;
+	}
+	dfs(0,0);
+
+	cout << result << '\n';
+}
