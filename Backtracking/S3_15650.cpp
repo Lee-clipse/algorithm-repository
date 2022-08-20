672009e155646f283e07047b1708b0821aa9c690
edac99 <79911816+edac99@users.noreply.github.com>
2022-08-20 14:53:28 +0900

    Solved S3_15650
    백트래킹 - 순열 조합

---
+++ Backtracking/S3_15650.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+
+int N, M;
+int arr[9];
+
+void permutation(int pos, int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) cout << arr[i] << " ";
+		cout << "\n";
+		return ;
+	}
+
+	for (int i = pos; i <= N; i++) {
+		arr[cnt] = i;
+		permutation(i+1, cnt+1);
+	}
+}
+
+int main() {
+    fastio;
+	cin >> N >> M;
+    permutation(1, 0);
+}
