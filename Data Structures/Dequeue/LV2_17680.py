65f647195f4d3a4a9349ebb74833eb1944b202b6
edac99 <79911816+edac99@users.noreply.github.com>
2023-01-10 14:43:35 +0900

    Solved LV2_17680
    collection deque 문제

---
+++ Data Structures/Dequeue/LV2_17680.py
@@ -0,0 +1,14 @@
+def solution(cacheSize, cities):
+    import collections
+    cache = collections.deque(maxlen = cacheSize)
+    time = 0
+    for city in cities:
+        city = city.lower()
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            time += 1
+        else:
+            cache.append(city)
+            time += 5
+    return time
